fdbda2452c6750c9dfddb8117392d90544f39233
Julian Carrasquillo <jul.carras@gmail.com>
2021-03-02 19:20:19 -0500

    Create http_client.py
    http.client exploration

---
+++ http_client.py
@@ -0,0 +1,27 @@
+import http.client
+import json
+
+phone = input_data['phone']
+conn = http.client.HTTPSConnection("api.callrail.com")
+
+payload = {"form_submission": {
+                               "company_id": "756876869",
+                               "referrer": "wikipedia_link", 
+                               "referring_url": "wikipedia.org/wiki/Carmen_Sandiego",
+                               "landing_page_url": "missingmonuments.com/info",
+                               "form_url": "missingmonuments.com/report/new",
+                               "form_data": {
+                                             "phone_number": "{phone}",
+                                             "report": "Carmen stole the Liberty Bell.",
+                                             "last_sighting": "Near Washington D.C."
+                                            }
+                              }
+          }
+headers = {
+  'Authorization': auth_key,
+  'Content-Type': 'application/json',
+  'Cookie': cookie_key
+}
+conn.request("POST", "/v3/a/ACC1f1e633474304b16b41941a520b5432a/form_submissions.json", json.dump(payload), json.dump(headers))
+res = conn.getresponse()
+data = res.read()
